0df38cd41fa90d870a7c91b17bc7cd01444911a5
jongturkson <jongturkson@gmail.com>
2024-11-06 16:46:47 +0100

    hello

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,17 +1,13 @@
-# test_app.py
-import unittest
-from app import app
+# app.py
+from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 
-class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_home_route(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "Hello level 400 FET, Quality Assurance!"})
+@app.route('/')
+def home():
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    app.run(debug=True)
