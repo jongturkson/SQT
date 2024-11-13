60e1beecc1da65e9f67cf6c08be089086def848f
Carlstorm <carlglainnyuysemo@gmail.com>
2024-11-13 16:43:09 +0100

    fix bug error 404

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/project')
+@app.route('/')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
